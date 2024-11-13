2e5b800c20aa2eaf2c6e69c3bf6faf5ee4b051a4
LucasPerbet <lucas.perbet1997@hotmail.com>
2024-11-13 19:48:26 +0100

    feat: :recycle: add OnInit and onDestroy with sub and unsub observable

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Hero } from '../../interfaces/hero';
-import { HEROES } from '../../data/mock-heroes';
 import { FormsModule } from '@angular/forms';
 import { NgFor } from '@angular/common';
 import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
+import { HeroService } from '../../services/hero.service';
+import { Subscription } from 'rxjs';
+
 
 
 @Component({
@@ -16,19 +18,24 @@ import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
 
 export class HeroesComponent implements OnInit, OnDestroy {
   
-  heroes = HEROES;
+  constructor(private heroService: HeroService) {}
+  
+  heroes: Hero[] = [];
   selectedHero?: Hero;
+  private heroesSubscription?: Subscription;
+  
+  ngOnInit(): void {
+   this.heroesSubscription = this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+  }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
 
-  ngOnInit(): void {
-    
-  }
   ngOnDestroy(): void {
-    
+    if(this.heroesSubscription){
+      this.heroesSubscription?.unsubscribe()
+    }
   }
-
 }
 
--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import { Hero } from '../interfaces/hero';
+import { HEROES } from '../data/mock-heroes';
+import { Observable, of  } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +9,9 @@ import { Injectable } from '@angular/core';
 export class HeroService {
 
   constructor() { }
+
+  getHeroes(): Observable<Hero[]> {
+    const heroes = of(HEROES);
+    return heroes;
+  }
 }
