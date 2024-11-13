c1622cf9092f7dd2685c2f743d5b9d1eab86c364
LucasPerbet <lucas.perbet1997@hotmail.com>
2024-11-13 10:25:08 +0100

    feat: :sparkles: details hero operationnal in the hero component

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -2,16 +2,25 @@ import { Component } from '@angular/core';
 import { Hero } from '../../interfaces/hero';
 import { HEROES } from '../../data/mock-heroes';
 import { FormsModule } from '@angular/forms';
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [FormsModule, NgFor],
+  imports: [FormsModule, NgFor, NgIf],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css'
 })
+
 export class HeroesComponent {
+  
   heroes = HEROES;
+  selectedHero?: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
 }
+
