f02df49a4d659dab968155d6cf05c64b708a2daa
LucasPerbet <lucas.perbet1997@hotmail.com>
2024-11-18 20:12:38 +0100

    feat: add some css

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,6 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { environment } from '../environments/environment';
+import { environment } from './environments/environment';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
